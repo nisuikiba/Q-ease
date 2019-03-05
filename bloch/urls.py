11f004dda8223adcdfd555c9250cdce2916aba20
nisuikiba <tanaka.sae832@mail.kyutech.jp>
2019-03-05 14:32:12 +0900

    add draw gates

--- bloch/urls.py
+++ bloch/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('index_ja/', views.jaview, name='jaview'), 
+    path('draw/', views.drawview, name='drawview'), 
     path('try/', views.tryview, name='tryview'), 
     path('challenge/', views.challengeview, name='challengeview'),
-    path('draw/', views.tryview, name='drawview'), 
 ]
